13804c249d74b1945515f545b6be4f7e10fc6bdf
Grzegorz Sztuczyński <sztuczynski.grzegorz@gmail.com>
2020-02-16 00:07:08 +0100

    refactor widget to use statefulwidget
    - manage simple state

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Lets see some images'),
         ),
+        body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
-          onPressed: () {
-            print('Hi there!');
-          },
+          onPressed: () => setState(() => counter += 1),
         ),
       ),
     );
